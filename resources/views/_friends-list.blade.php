58a8197ea93935472ca88d689aaa5685ebe33e86
Lanza <lanzathm@gmail.com>
2020-08-02 18:45:44 -0300

    :sparkles: Link to profile added

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -4,14 +4,16 @@
     {{-- foreach loop to render friends from index 1 to 8 --}}
     @foreach (auth()->user()->follows as $user)
         <li class="mb-4">
-            <div class="flex items-center text-sm">
-                <img
-                    src="{{ $user->avatar }}"
-                    alt="avatar"
-                    class="rounded-full mr-2"
-                >
+            <div>
+                <a href="{{ route('profile', $user) }}" class="flex items-center text-sm">
+                    <img
+                        src="{{ $user->avatar }}"
+                        alt="avatar"
+                        class="rounded-full mr-2"
+                    >
 
-                {{ $user->name }}
+                    {{ $user->name }}
+                </a>
             </div>
         </li>
     @endforeach
--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -3,15 +3,21 @@
 --}}
 <div class="flex p-4 border-b border-gray-400">
     <div class="mr-2 flex-shrink-0">
-        <img
-            src="{{ $tweet->user->avatar }}"
-            alt="avatar"
-            class="rounded-full mr-2"
-        >
+        <a href="{{ route('profile', $tweet->user->name) }}">
+            <img
+                src="{{ $tweet->user->avatar }}"
+                alt="avatar"
+                class="rounded-full mr-2"
+            >
+        </a>
     </div>
 
     <div>
-        <h5 class="font-bold mb-2"> {{ $tweet->user->name }} </h5>
+        <h5 class="font-bold mb-2">
+            <a href="{{ route('profile', $tweet->user) }}">
+                {{ $tweet->user->name }}
+            </a>
+        </h5>
 
         <p class="text-sm">
             {{ $tweet->body }}
